67af8a31fcf5156f0177ce2a701aadf55569ecd9
Daniel Harrison <harrison92d@gmail.com>
2022-05-12 14:35:42 +0100

    Failing test for single C item

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -37,6 +37,14 @@ public class CheckoutTests
         checkout.Scan("B");
         Assert.That(checkout.GetTotalPrice(), Is.EqualTo(60));
     }
+
+    [Test]
+    public void Can_calculate_price_for_single_C()
+    {
+        var checkout = new Checkout();
+        checkout.Scan("C");
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(20));
+    }
 }
 
 public interface ICheckout
