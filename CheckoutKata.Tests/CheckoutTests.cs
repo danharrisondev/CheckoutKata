c0f2292bf54f0d01d9311d9494e96d81d31e31e5
Daniel Harrison <harrison92d@gmail.com>
2022-05-12 14:24:06 +0100

    Passing test for a single A item

---
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace CheckoutKata.Tests;
+
+public class CheckoutTests
+{
+    [Test]
+    public void Can_calculate_price_for_A()
+    {
+        var checkout = new Checkout();
+        checkout.Scan("A");
+        Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+    }
+}
+
+public interface ICheckout
+{
+    void Scan(string item);
+    int GetTotalPrice();
+}
+
+public class Checkout : ICheckout
+{
+    public int GetTotalPrice()
+    {
+        return 50;
+    }
+
+    public void Scan(string item)
+    {
+        
+    }
+}
