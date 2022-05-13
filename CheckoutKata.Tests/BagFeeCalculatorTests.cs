2ad37ceda7c4c63f5f030ecbe125dbe0ac8a026f
Daniel Harrison <harrison92d@gmail.com>
2022-05-13 09:51:56 +0100

    Use file-scoped namespace

--- CheckoutKata.Tests/BagFeeCalculatorTests.cs
+++ CheckoutKata.Tests/BagFeeCalculatorTests.cs
@@ -1,21 +1,20 @@
 ﻿using NUnit.Framework;
 
-namespace CheckoutKata.Tests
+namespace CheckoutKata.Tests;
+
+public class BagFeeCalculatorTests
 {
-    public class BagFeeCalculatorTests
+    [TestCase(1, 5)]
+    [TestCase(2, 5)]
+    [TestCase(3, 5)]
+    [TestCase(4, 5)]
+    [TestCase(5, 5)]
+    [TestCase(6, 10)]
+    [TestCase(11, 15)]
+    [TestCase(16, 20)]
+    public void Can_calculate_bag_fee(int itemCount, int expectedPrice)
     {
-        [TestCase(1, 5)]
-        [TestCase(2, 5)]
-        [TestCase(3, 5)]
-        [TestCase(4, 5)]
-        [TestCase(5, 5)]
-        [TestCase(6, 10)]
-        [TestCase(11, 15)]
-        [TestCase(16, 20)]
-        public void Can_calculate_bag_fee(int itemCount, int expectedPrice)
-        {
-            var calculator = new BagFeeCalculator();
-            Assert.That(calculator.GetBagFee(itemCount), Is.EqualTo(expectedPrice));
-        }
+        var calculator = new BagFeeCalculator();
+        Assert.That(calculator.GetBagFee(itemCount), Is.EqualTo(expectedPrice));
     }
 }
